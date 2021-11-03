9f55e512892faec3718d0ce33ed22a762be9eac7
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-03 19:32:54 +0100

    fix: ahoj alias

--- cmd/ahoj/ahoj.go
+++ cmd/ahoj/ahoj.go
@@ -10,7 +10,7 @@ import (
 var Cmd = &cobra.Command{
 	Use:     "ahoj",
 	Short:   "Say: Ahoj!",
-	Aliases: []string{"h"},
+	Aliases: []string{"a"},
 	Args:    cobra.NoArgs,
 	Run: func(c *cobra.Command, args []string) {
 		fmt.Println("Ahoj!")
