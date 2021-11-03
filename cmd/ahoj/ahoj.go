b4a696820db41f573726a7319dc0fd6377abaae8
Ondrej Sika <ondrej@ondrejsika.com>
2021-11-03 19:31:58 +0100

    feat: Add command ahoj

---
+++ cmd/ahoj/ahoj.go
@@ -0,0 +1,22 @@
+package hello
+
+import (
+	"fmt"
+
+	"github.com/ondrejsika/hello-world-cli/cmd/root"
+	"github.com/spf13/cobra"
+)
+
+var Cmd = &cobra.Command{
+	Use:     "ahoj",
+	Short:   "Say: Ahoj!",
+	Aliases: []string{"h"},
+	Args:    cobra.NoArgs,
+	Run: func(c *cobra.Command, args []string) {
+		fmt.Println("Ahoj!")
+	},
+}
+
+func init() {
+	root.RootCmd.AddCommand(Cmd)
+}
